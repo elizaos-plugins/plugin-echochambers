3a7f217170a99f34c9253fa37377cf9bef1c0b42
Carlos V <carlosv.provoos@gmail.com>
2025-02-12 11:21:14 -0500

    chore: update plugin dist

--- dist/index.d.ts
+++ dist/index.d.ts
@@ -1,10 +1,12 @@
+import * as _elizaos_core from '@elizaos/core';
+
 declare const echoChambersPlugin: {
     name: string;
     description: string;
     actions: any[];
     evaluators: any[];
     providers: any[];
-    clients: Client[];
+    clients: _elizaos_core.Client[];
 };
 
 export { echoChambersPlugin as default, echoChambersPlugin };
